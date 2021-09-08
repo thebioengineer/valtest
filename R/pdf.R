3284ebb07bfc91c925c8e298c78cf4fab3716588
thebioengineer <ellishughes@live.com>
2021-09-08 07:14:16 -0700

    clarify tests and add ggsave example

--- R/pdf.R
+++ R/pdf.R
@@ -16,19 +16,35 @@ compare_pdf_content <- function(old, new){
 #' @param path path to pdf to have contents read
 #' @returns pdf contents as serialized image
 #'
-#' @importFrom magick image_read_pdf image_append image_write
+#' @importFrom magick image_read_pdf image_append image_write image_info
 #' @importFrom brio read_file_raw
 #'
 #' @noRd
 #' @keywords internal
 read_pdf_raw <- function(path){
-  tmp_image <- tempfile(fileext = ".png")
-  pdf_to_png(path, tmp_image)
+  tmp_image <- pdf_to_png(path, stack = TRUE)
   brio::read_file_raw(tmp_image)
 }
 
-pdf_to_png <- function(path, output){
+pdf_to_png <- function(path, output = tempfile(fileext = ".png"), stack = FALSE){
   pdf_image <- magick::image_read_pdf(path = path,density = 72)
-  magick::image_write(magick::image_append(pdf_image,stack = TRUE), path = output,format = "png")
-}
 
+  if(stack){
+    pdf_image <- magick::image_append(pdf_image,stack = TRUE)
+  }else{
+    output <- file.path(
+      dirname(output),
+      paste0(
+        tools::file_path_sans_ext(basename(output)),
+        "_",
+        seq_len(nrow(magick::image_info(pdf_image))),
+        ".",
+        tools::file_ext(output)
+      )
+    )
+  }
+  for(i in seq_len(nrow(magick::image_info(pdf_image)))){
+    magick::image_write(pdf_image[i],path = output[i],format = "png")
+  }
+  return(output)
+}
--- tests/testthat/test-pdf_comparison.R
+++ tests/testthat/test-pdf_comparison.R
@@ -1,4 +1,4 @@
-test_that("compare_pdf_content returns TRUE when PDF image files should match", {
+test_that("compare_pdf_content returns TRUE when PDF image files should match - pdf()", {
 
   test_file_1 <- tempfile()
   test_file_2 <- tempfile()
@@ -17,7 +17,27 @@ test_that("compare_pdf_content returns TRUE when PDF image files should match", 
 
 })
 
-test_that("compare_pdf_content returns FALSE when PDF image files shouldn't match", {
+test_that("compare_pdf_content returns TRUE when PDF image files should match - ggsave()", {
+
+  test_file_1 <- tempfile(fileext = ".pdf")
+  test_file_2 <- tempfile(fileext = ".pdf")
+
+  tmp_gg <- ggplot2::ggplot(data.frame(x = c(1,2,3,4))) +
+    ggplot2::geom_point(
+      ggplot2::aes(x = x, y = x)
+    )
+
+  suppressMessages({
+    ggplot2::ggsave(test_file_1, plot = tmp_gg)
+    ggplot2::ggsave(test_file_2, plot = tmp_gg)
+  })
+
+  expect_true(compare_pdf_content(test_file_1, test_file_1))
+  expect_true(compare_pdf_content(test_file_1, test_file_2))
+
+})
+
+test_that("compare_pdf_content returns FALSE when PDF image files shouldn't match - png()", {
 
   test_file_1 <- tempfile()
   test_file_2 <- tempfile()
@@ -30,7 +50,29 @@ test_that("compare_pdf_content returns FALSE when PDF image files shouldn't matc
   plot(c(1,2,3,5))
   dev.off()
 
-  expect_true(test_file_1 != test_file_2)
+  expect_false(compare_pdf_content(test_file_1, test_file_2))
+
+})
+
+test_that("compare_pdf_content returns FALSE when PDF image files shouldn't match - ggsave()", {
+
+  test_file_1 <- tempfile(fileext = ".pdf")
+  test_file_2 <- tempfile(fileext = ".pdf")
+
+  tmp_gg <- ggplot2::ggplot(data.frame(x = c(1,2,3,4))) +
+    ggplot2::geom_point(
+      ggplot2::aes(x = x, y = x)
+    )
+
+  tmp_gg2 <- ggplot2::ggplot(data.frame(x = c(1,2,3,4,5))) +
+    ggplot2::geom_point(
+      ggplot2::aes(x = x, y = x)
+    )
+  suppressMessages({
+    ggplot2::ggsave(test_file_1, plot = tmp_gg)
+    ggplot2::ggsave(test_file_2, plot = tmp_gg2)
+  })
+
   expect_false(compare_pdf_content(test_file_1, test_file_2))
 
 })
@@ -44,7 +86,6 @@ test_that("compare_pdf_content returns TRUE when Document pdf files should match
   writeLines(c(
     "---",
     "title: \"Untitled\"",
-    "date: \"9/7/2021\"",
     "output: pdf_document",
     "---",
     "",
@@ -77,12 +118,60 @@ test_that("compare_pdf_content returns TRUE when Document pdf files should match
     })
   })
 
-  expect_true(output_pdf_1 != output_pdf_2)
   expect_true(compare_pdf_content(output_pdf_1, output_pdf_1))
   expect_true(compare_pdf_content(output_pdf_1, output_pdf_2))
 
 })
 
+test_that("compare_pdf_content returns FALSE when Document pdf files shouldn't match", {
+
+  template_rmd_1 <- tempfile(fileext = ".Rmd")
+  template_rmd_2 <- tempfile(fileext = ".Rmd")
+  output_pdf_1 <- tempfile(fileext = ".pdf")
+  output_pdf_2 <- tempfile(fileext = ".pdf")
+
+  template_rmd_text <- c(
+    "---",
+    "title: \"Untitled\"",
+    "output: pdf_document",
+    "---",
+    "",
+    "## R Markdown",
+    "",
+    "This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.",
+    "",
+    "When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:",
+    "",
+    "```{r cars}",
+    "summary(cars)",
+    "```",
+    "",
+    "## Including Plots",
+    "",
+    "You can also embed plots, for example:",
+    "",
+    "```{r pressure, echo=FALSE}",
+    "plot(pressure)",
+    "```",
+    "",
+    "Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.",
+    ""
+  )
+
+  writeLines(template_rmd_text, template_rmd_1)
+
+  writeLines(c(template_rmd_text,"## New Header","","New text line"), template_rmd_2)
+
+  quiet <- capture.output({
+    suppressMessages({
+      rmarkdown::render(template_rmd_1, output_file = output_pdf_1)
+      rmarkdown::render(template_rmd_2, output_file = output_pdf_2)
+    })
+  })
+
+  expect_false(compare_pdf_content(output_pdf_1, output_pdf_2))
+
+})
 
 test_that("compare_pdf_content works in snapshotting", {
 
